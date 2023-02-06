733eb233bc1dd433dca6a833770ab006d4964263
u418572 <u418572@kimo.com>
2023-02-06 19:10:31 +0800

    complate app.use(express.static('public')) app.get('/', (req, res) app.get('/restaurants/:restaurant_id', (req, res)

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 // include express
 const express = require('express')
+const exphbs = require('express-handlebars')
+// require restaurant.json
+const restaurants = require('./restaurant.json')
 const app = express()
 const port = 3000
+
 // include handlebars
-const exphbs = require('express-handlebars')
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+
+
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('happy new year')
+  res.render('index', {restaurants: restaurants.results})
 })
 
+// render restaurant/
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  console.log('req params', req.params.restaurant_id)
+  const restaurant = restaurants.results.find(item => item.id.toString()==req.params.restaurant_id)
+   console.log('this:',restaurant)
+  res.render('show', {restaurant: restaurant})
+})
 app.listen(port ,() => {
   console.log(`Express is listening on http://localhost:${port}`)
 })
